2af5e46108773f724a3b24d302a2d3c51e57adc4
raulgalis <rgalisteo2000@gmail.com>
2022-04-21 19:34:56 +0200

    2

--- 2021/Day3/part2.py
+++ 2021/Day3/part2.py
@@ -1,7 +1,11 @@
-with open('input') as i:
-    gamma = ""
-    epsilon = ""
-    file = i.read().split()
-    TotalLen = len(file)
-    ElementLen = len(file[0])
-print(file)
+f = open('input', mode="rt", encoding="utf-8")
+o2 = []
+co2 = []
+for l in f:
+    o2.append(int(l))
+    co2.append(int(l))
+f.close()
+
+#TODO: if len list = 2 ...
+#TODO: def bitsCounter
+#TODO: def clearList
