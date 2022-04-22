b0347051f71c0e5bf12a36afa88a5b293b695fdc
raulgalis <rgalisteo2000@gmail.com>
2022-04-22 21:24:22 +0200

    def

--- 2021/Day3/part2.py
+++ 2021/Day3/part2.py
@@ -2,9 +2,24 @@ f = open('input', mode="rt", encoding="utf-8")
 o2 = []
 co2 = []
 for l in f:
-    o2.append(int(l))
-    co2.append(int(l))
+    o2.append((l))
+    co2.append((l))
 f.close()
+
+def getBits(list, position):
+    c0 = 0
+    c1 = 0
+    for i in list:
+        if i[position] == "0":
+            c0 += 1
+        else:
+            c1 += 1
+    if c0 > c1:
+        return 0, position
+    else:
+        return 1, position
+x, y = getBits(o2, 3)
+print(x, y)
 
 #TODO: if len list = 2 ...
 #TODO: def bitsCounter
