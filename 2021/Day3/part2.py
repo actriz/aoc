56bf7dee1f9187ff3d0216e1f6c2e0a45dd0ced2
raulgalis <rgalisteo2000@gmail.com>
2022-04-23 23:59:56 +0200

    done

--- 2021/Day3/part2.py
+++ 2021/Day3/part2.py
@@ -1,34 +1,56 @@
 f = open('input')
 text = f.read().split()
-o2 = []
-co2 = []
-for l in text:
-    o2.append((l))
-    co2.append((l))
+o2 = co2 = text
 f.close()
 
-def getBits(list, position):
+def getBitso2(listado, position):
     c0 = 0
     c1 = 0
-    for i in list:
+    for i in listado:
         if i[position] == "0":
             c0 += 1
         else:
             c1 += 1
     if c0 > c1:
         return 0, position
+    elif c0 == c1:
+        return 1, position
     else:
         return 1, position
 
-def clearList(list, position, bit):
-    return [x for x in list if int(x[position]) == bit]
+def getBitsco2(listado, position):
+    c0 = 0
+    c1 = 0
+    for i in listado:
+        if i[position] == "0":
+            c0 += 1
+        else:
+            c1 += 1
+    if c0 > c1:
+        return 1, position
+    elif c0 == c1:
+        return 0, position
+    else:
+        return 0, position
 
-x, y = getBits(o2, 0)
-clearList(o2, y, x)
+def clearList(listado, bit, position):
+    return [x for x in listado if int(x[position]) == bit]
 
-# if len list = 2 ...
-# for i in range(2)
-    # for x in range(len(list[x]))
-        # if len list = 2 ...
-        # def
-        # def
+for x in range(len(o2[0])):
+    if len(o2) == 1:
+        break
+    else:
+        b, p = getBitso2(o2, x)
+        o2 = clearList(o2, b, p)
+
+for x in range(len(co2[0])):
+    if len(co2) == 1:
+        break
+    else:
+        b, p = getBitsco2(co2, x)
+        co2 = clearList(co2, b, p)
+
+n_o2 = int(o2[0],2)
+n_co2 = int(co2[0],2)
+r = n_o2 * n_co2
+print(r)
