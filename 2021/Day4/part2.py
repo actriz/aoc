5f87a521c1e2fe712d37bd792016c82cff193b29
raulgalis <rgalisteo2000@gmail.com>
2022-05-27 20:49:50 +0200

    part2Done

--- 2021/Day4/part2.py
+++ 2021/Day4/part2.py
@@ -1,1 +1,62 @@
-TODO: The last winner.
+import numpy as np
+
+f = open('input').read().split()
+
+def createLists(listado):
+    n = 25
+    numbers = listado[0].split(',')
+    cartons = [listado[i:i+n] for i in range(1, len(listado), n)]
+    return numbers, cartons
+
+def correctCartons(listado):
+    finalCartons = []
+    n = 5
+    for x in range(len(listado)):
+        tempList = listado[x]
+        output = [tempList[i:i+n] for i in range(0, len(tempList), n)]
+        finalCartons.append(output)
+    return finalCartons
+
+def markBingo(integer, game):
+    for c in range(len(game)):
+        for f in range(len(game[c])):
+            for n in range(len(game[c][f])):
+                if game[c][f][n] == integer:
+                    game[c][f][n] = 'X'
+    return game
+
+def checkWin(joses, n):
+    for c in joses:
+        for f in c:
+            if all(i == 'X' for i in f):
+                cartons.remove(c)
+                ws.append(c)
+                ns.append(n)
+
+    for c in joses:
+        a = [f for f in c]
+        x = np.transpose(a)
+        for i in x:
+            if all(n == 'X' for n in i):
+                cartons.remove(c)
+                ws.append(c)
+                ns.append(n)
+
+def winner(sufle, mawik):
+    s = 0
+    for x in sufle:
+        for n in x:
+            if n != 'X':
+                s += int(n)
+    print(s*int(mawik))
+
+numbers, c = createLists(f)
+cartons = correctCartons(c)
+ws = []
+ns = []
+
+for i in range(len(numbers)):
+    markBingo(numbers[i], cartons)
+    checkWin(cartons, numbers[i])
+
+winner(ws[-1], ns[-1])
