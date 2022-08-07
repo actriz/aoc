c6a62d1a85aed4a7f253f52d99cf1deab8efcb0d
raulgalis <rgalisteo2000@gmail.com>
2022-08-07 18:48:20 +0200

    ñe

--- 2021/Day5/part1.py
+++ 2021/Day5/part1.py
@@ -5,14 +5,14 @@
 #   ]
 f = open('input').read().splitlines()
 
-def gRanges(l):
+def ranges(l):
     r0 = [_.split('->') for _ in l]
     r1 = [e.replace(' ','') for i in r0 for e in i]
     r2 = [_.split(',') for _ in r1]
     r3 = [r2[_:_+2] for _ in range(0, len(r2), 2)]
     return r3
 
-def cMatrix(xy):
+def matrix(xy):
     x = 0
     y = 0
     for i in xy:
@@ -21,12 +21,11 @@ def cMatrix(xy):
                 x = int(e[0])
             if int(e[1]) > y:
                 y = int(e[1])
-    m = [['.' for _ in range(x)] for _ in range(y)]
+    m = [[0 for _ in range(x)] for _ in range(y)]
     return m
 
-def gInterlude(jose):
-    print(jose)
-    interlude = [jose[0], jose[1]]
+def interlude(jose):
+    interlude = [[int(n1), int(n2)] for n1, n2 in jose]
     x1 = int(jose[0][0])
     y1 = int(jose[0][1])
     x2 = int(jose[1][0])
@@ -53,18 +52,27 @@ def gInterlude(jose):
     else:
         ys.append(y1)
 
-    for a, b in enumerate(xs):
-        interlude.append([b, ys[a % len(ys)]])
+    try:
+        for a, b in enumerate(xs):
+            interlude.append([b, ys[a % len(ys)]])
+    except:
+        for a, b in enumerate(ys):
+            interlude.append([b, xs[a % len(xs)]])
+    
+    return interlude
 
-    print(interlude)
+def mark(mawik, aron):
+    for e in mawik:
+        y, x = e
+        try:
+            aron[y][x-1] += 1
+        except:
+            aron[y-1][x] += 1
 
-    # List comprehension x and y
-    # max/min
-    # zip
+r = ranges(f)
+m = matrix(r)
 
-r = gRanges(f)
-m = cMatrix(r)
-
-for i in range(2):
-    gInterlude(r[i])
-    print()
+for i in range(len(r)):
+    i = interlude(r[i])
+    mark(i, m)
+print(m)
