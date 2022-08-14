3034272d35323adf0830aaad29635e3d05e07455
raulgalis <rgalisteo2000@gmail.com>
2022-08-14 19:20:32 +0200

    not yet

--- 2021/Day5/part1.py
+++ 2021/Day5/part1.py
@@ -31,28 +31,67 @@ def matrix(xy):
     m = [[0 for _ in range(x)] for _ in range(y)]
     return m
 
+def xyz(x1,y1,x2,y2):
+    xs = []
+    ys = []
+    l = 0
+    if x1==x2:
+        xs.append(x1)
+    if y1==y2:
+        ys.append(y1)
+    if x1<x2:
+        for i in range(x1, x2+1):
+            xs.append(i)
+    if y1<y2:
+        for i in range(y1, y2+1):
+            ys.append(i)
+    if x1>x2:
+        for i in range(x1, x2-1,-1):
+            xs.append(i)
+    if y1>y2:
+        for i in range(y1, y2-1,-1):
+            ys.append(i)
+    if len(xs)>len(ys):
+        l = len(xs)
+    else:
+        l = len(ys)
+    return xs, ys, l
+
 def interlude(jose):
     interlude = []
-    [[x1,y1],[x2,y2]] = jose
+    x1 = int(jose[0][0])
+    y1 = int(jose[0][1])
+    x2 = int(jose[1][0])
+    y2 = int(jose[1][1])
+    xlist, ylist, l = xyz(x1,y1,x2,y2)
+    
+    for i in range(l):
+        x=0
+        y=0
+        try:
+            x=xlist[i]
+            y=ylist[i]
+        except IndexError:
+            if len(xlist)==1:
+                x=xlist[0]
+            else:
+                x=xlist[i]
+            if len(ylist)==1:
+                y=ylist[0]
+            else:
+                y=ylist[i]
+        
+        interlude.append((x,y))
+    return interlude
 
-    if int(y1)==int(y2):
-        if int(x1)<int(x2):
-            for i in range(int(x1),int(x2)+1):
-                interlude.append([i, y1])
-        else:
-            for i in range(int(x2),int(x1)+1):
-                interlude.append([i, y1])
-
-    if int(x1)==int(x2):
-        if int(y1)<int(y2):
-            for i in range(int(y1),int(y2)+1):
-                interlude.append([i, x1])
-        else:
-            for i in range(int(y2),int(y1)+1):
-                interlude.append([i, x1])
+def mark(mawik, aron):
+    for n in aron:
+        x, y = n
+        mawik[y-1][x-1] += 1
 
 r = ranges(f)
 m = matrix(r)
 
 for i in range(len(r)):
-   interlude(r[i])
+   i = interlude(r[i])
+   mark(m, i)
