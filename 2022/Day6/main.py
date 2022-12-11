fce558464ab1f7928616552e2d5beb7f661cc6ee
raulgalis <rgalisteo2000@gmail.com>
2022-12-11 14:19:18 +0100

    day6

---
+++ 2022/Day6/main.py
@@ -0,0 +1,10 @@
+f = open('input').read()
+
+def marker(t, n):
+    for i in range(len(t)-n):
+        if len(set(t[i:i+n]))==n: 
+            print(i+n)
+            break
+
+marker(f,4)
+marker(f,14)
